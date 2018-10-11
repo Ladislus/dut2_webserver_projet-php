b2a7ed0871577085e887a1bd3eca6eab35ea938c
Ladislas WALCAK <walcak.ladislas@gmail.com>
2018-10-11 15:07:48 +0200

    ajout des liens clickable vers les templates

--- rechercher.php
+++ rechercher.php
@@ -10,20 +10,20 @@
       printf("Échec de la connexion : %s\n", $e->getMessage());
       exit(); }
 
-#Récupération des infos des différentes barres de recherche
-$recherche = $_POST['recherche'];
+  #Récupération des infos des différentes barres de recherche
+  $recherche = $_POST['recherche'];
 
-#Query "JEU" dans la BD
-$sql = "SELECT * FROM JEU WHERE NOMJ LIKE '%$recherche%'";
-#Lancement de $sql
-if(!$connexion->query($sql)) echo "Pb d'accès au JEU";
-else {
-  if(($connexion->query($sql)->rowCount() == 0) or empty($recherche)) { echo ""; }
+  #Query "JEU" dans la BD
+  $sql = "SELECT * FROM JEU WHERE NOMJ LIKE '%$recherche%'";
+  #Lancement de $sql
+  if(!$connexion->query($sql)) echo "Pb d'accès au JEU";
   else {
-    echo "<h1> JEUX </h1>";
-  # Affichage des jeux
-  foreach ($connexion->query($sql) as $row) {
-    echo $row['NOMJ']."<br/>\n"; }}}
+    if(($connexion->query($sql)->rowCount() == 0) or empty($recherche)) { echo ""; }
+    else {
+      echo "<h1> JEUX </h1>";
+    # Affichage des jeux
+    foreach ($connexion->query($sql) as $row) {
+      echo "<a href='template/jeu.php?name=".urlencode($row['IDJ'])."'>".$row['NOMJ']."</a><br/>\n"; }}}
 
 ###########################################################################
 
@@ -37,7 +37,9 @@ else {
         echo "<h1> EDITEUR </h1>";
     # Affichage des éditeurs
     foreach ($connexion->query($sql) as $row) {
-      echo $row['NOME']."<br/>\n"; }}}
+      echo "<a href='template/editeur.php?name=".urlencode($row['IDE'])."'>".$row['NOME']."</a><br/>\n"; }}}
+
+###########################################################################
 
   #Query "GENRE" dans la BD
   $sql = "SELECT * FROM GENRE WHERE NOMGENRE LIKE '%$recherche%'";
@@ -49,7 +51,9 @@ else {
         echo "<h1> GENRE </h1>";
     # Affichage des genres
     foreach ($connexion->query($sql) as $row) {
-      echo $row['NOMGENRE']."<br/>\n"; }}}
+      echo "<a href='template/genre.php?name=".urlencode($row['IDG'])."'>".$row['NOMGENRE']."</a><br/>\n"; }}}
+
+###########################################################################
 
   #Query "THEME" dans la BD
   $sql = "SELECT * FROM THEME WHERE NOMTHEME LIKE '%$recherche%'";
@@ -58,8 +62,8 @@ else {
   else {
     if(($connexion->query($sql)->rowCount() == 0) or empty($recherche)) { echo ""; }
     else {
-        echo "<h1> GENRE </h1>";
+        echo "<h1> THEME </h1>";
     # Affichage des jeux
     foreach ($connexion->query($sql) as $row) {
-      echo $row['NOMTHEME']."<br/>\n"; }}}
+      echo "<a href='template/theme.php?name=".urlencode($row['IDT'])."'>".$row['NOMTHEME']."</a><br/>\n"; }}}
 ?>
