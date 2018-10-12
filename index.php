ec8b022efcc83452ccf98d6fd92ae2a25515e9a7
Alexis Dupre <adupre@info24-03.iut45.univ-orleans.fr>
2018-10-12 13:53:42 +0200

    Refonte du style de la page

--- index.php
+++ index.php
@@ -16,16 +16,16 @@
 
     <!-- Bannière -->
     <header>
-      <img src="image/banniere.jpg">
+      <div id="leftside"><img src="image/logo.png">
+      <h1>GameLand<h1></div>
+      <!-- Liens -->
+      <div id="liens">
+        <a id="lien" href="listeEditeur.php">Accéder aux éditeurs</a>
+        <a id="lien" href="listeJeu.php">Accéder aux jeux</a>
+        <a id="lien" href="listeTheme.php">Accéder aux thèmes</a>
+        <a id="lien" href="listeGenre.php">Accéder aux genres</a>
+      </div>
     </header>
-
-    <!-- Liens -->
-    <div id="liens">
-      <a id="lien" href="listeEditeur.php">Accéder aux éditeurs</a>
-      <a id="lien" href="listeJeu.php">Accéder aux jeux</a>
-      <a id="lien" href="listeTheme.php">Accéder aux thèmes</a>
-      <a id="lien" href="listeGenre.php">Accéder aux genres</a>
-    </div>
 
     <div id="corps">
     <!-- Barre de recherche -->
