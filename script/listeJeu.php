62543155ab27e94f98f004f41e5a92f71fca774e
Alexis Dupre <adupre@info27-24.iut45.univ-orleans.fr>
2018-10-18 16:54:01 +0200

    Changement du CSS + Petit bug sur le p

--- script/listeJeu.php
+++ script/listeJeu.php
@@ -27,8 +27,12 @@
       echo "<div class=\"objet\">\n";
       foreach($connexion->query($sql) as $row) {
         echo "<div class=\"item\">\n";
-        echo "<img src=\"../image/jeu/".$row['IDJ'].".png\">\n";
-        echo "<a href='../template/jeu.php?id=".urlencode($row['IDJ'])."'>".$row['NOMJ']."</a><br/>\n";
+          echo "<div class='image'>\n";
+            echo "<img src=\"../image/jeu/".$row['IDJ'].".png\">\n";
+          echo "</div>";
+          echo "<div class='text'>\n";
+            echo "<a href='../template/jeu.php?id=".urlencode($row['IDJ'])."'>".$row['NOMJ']."</a><br/>\n";
+          echo "</div>";
         echo "</div><br>\n"; }
       echo "</div>"; }}
 
